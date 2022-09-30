18653bd0eb68c1b5d3680a410537677314e91f1a
Muiz <muiz.idn@gmail.com>
2022-09-30 13:20:37 +0700

    Run with expressJS

---
+++ index.js
@@ -0,0 +1,9 @@
+// https://www.digitalocean.com/community/tutorials/nodejs-serving-static-files-in-express
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.static("."));
+
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
