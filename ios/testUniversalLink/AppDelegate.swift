3e68732d992ce03405d730214425c24b01fc40f5
Muiz <muiz.idn@gmail.com>
2022-09-30 16:54:09 +0700

    Add ios project

---
+++ ios/testUniversalLink/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  testUniversalLink
+//
+//  Created by M on 26/09/22.
+//
+
+import UIKit
+
+var UNIVERSAL_LINK = "MUST CHANGE WHEN TAP UNIVERSAL LINK"
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+              let incomingURL = userActivity.webpageURL,
+              let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
+          return false
+        }
+        
+        UNIVERSAL_LINK = components.path ?? "NO_LINK"
+        
+        window?.rootViewController?.viewWillAppear(true)
+        
+        return false
+    }
+}
---
+++ ios/testUniversalLink/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  testUniversalLink
+//
+//  Created by M on 26/09/22.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        label.text = UNIVERSAL_LINK
+    }
+}
