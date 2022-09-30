2be774ade415afec12781e405f7252cd4a28e522
Muiz <muiz.idn@gmail.com>
2022-09-30 08:56:37 +0700

    Add working AASA sample

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	indexFs := http.FileServer(http.Dir("./"))
+	http.Handle("/", indexFs)
+	staticFs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", staticFs))
+	wellKnownFs := http.Dir("./.well-known")
+	http.Handle("/apple-app-site-association", http.FileServer(wellKnownFs))
+	http.Handle("/.well-known/", http.StripPrefix("/.well-known/", http.FileServer(wellKnownFs)))
 
 	log.Print("Listening on: 3000")
 	err := http.ListenAndServe(":3000", nil)
