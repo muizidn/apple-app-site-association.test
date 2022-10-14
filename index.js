1d2b4998ab0fc306d0d008534f50b73de066203e
Muiz <muiz.idn@gmail.com>
2022-10-14 15:06:57 +0700

    Add route handler to index.js

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ app.use(express.static("public"));
 app.use(express.static("."));
 
 app.get("/car-list", (req, res) => {
-    res.send("Car List Must redirect to app");
+    res.send("Car list not included in AASA filter");
+});
+
+app.get("/cars", (req, res) => {
+    res.send("Cars Must redirect to app");
+});
+
+app.get("/promo/123", (req, res) => {
+    res.send("Promo/{ANY_ID} Must redirect to app, if not then PULL this web will show OPEN button");
+});
+
+app.get("/email", (req, res) => {
+    res.send("/email Must NOT redirect to app, Pull web should not show OPEN button");
 });
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
