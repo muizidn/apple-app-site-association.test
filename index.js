b132a35eddf905309721d8a62b63cf7d6a11da34
Muiz <muiz.idn@gmail.com>
2022-10-14 15:12:47 +0700

    Update promo router handler to match all id

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ app.use(express.static("public"));
 app.use(express.static("."));
 
 app.get("/car-list", (req, res) => {
-    res.send("Car list not included in AASA filter");
+    res.send("<h1>Car list not included in AASA filter</h1>");
 });
 
 app.get("/cars", (req, res) => {
-    res.send("Cars Must redirect to app");
+    res.send("<h1>Cars Must redirect to app</h1>");
 });
 
-app.get("/promo/123", (req, res) => {
-    res.send("Promo/{ANY_ID} Must redirect to app, if not then PULL this web will show OPEN button");
+app.get("/promo/*", (req, res) => {
+    res.send("<h1>Promo with {ANY_ID} Must redirect to app, if not then PULL this web will show OPEN button</h1>");
 });
 
 app.get("/email", (req, res) => {
-    res.send("/email Must NOT redirect to app, Pull web should not show OPEN button");
+    res.send("<h1>Email Must NOT redirect to app, Pull web should not show OPEN button</h1>");
 });
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
